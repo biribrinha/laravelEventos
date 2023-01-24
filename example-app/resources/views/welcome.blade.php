8051585060ae362c29678f8e081c93b598ec86d6
Left Tecnologia <ediel.rangel@left.dev.br>
2023-01-24 09:15:04 -0300

    merge

--- example-app/resources/views/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-
-    @extends('layouts.app')
-
-    @section('title', 'home')
-
-    @section('content')   
-                <h1>bem vindo</h1>                         
-                <a href="{{ url('/pokemons') }}">Ver pokemons</a>
-    @endsection

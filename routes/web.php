f598ff23c6b5ca8b1253252e9866d569ef5107ab
juliabergman <juliabergman523@gmail.com>
2023-01-25 05:53:22 -0300

    feat: rotas e views

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/home', 'home/index');
+
+Route::fallback(function () {
+    return "Erro ao localizar a rota! :(";
+});
